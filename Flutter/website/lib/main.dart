bd560f199d3ce06e302c78b82ac5f46b24de0514
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-12-10 23:47:10 +0530

    add inter font

--- Flutter/website/lib/main.dart
+++ Flutter/website/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.green,
+         fontFamily: 'Inter',
       ),
       home: HomePage(),
     );
